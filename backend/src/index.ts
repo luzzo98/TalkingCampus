a52ed1ccaaa43f5f365756eff23b0bd828cf7c6e
Christian D'Errico <christian.derrico@studio.unibo.it>
2021-09-29 10:57:09 +0200

    changed mongo connection string

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ app.use(express.json());
 //...
 
 //connessione a mongo db
-mongoose.connect('mongodb://localhost:27017/talking-campus');
+mongoose.connect('mongodb://mongo:27017/talking-campus');
 
 //il server è on
 app.listen(PORT, function () {
