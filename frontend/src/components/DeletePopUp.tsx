1eda4c6d8988f68fb8234239a3ff05f96ef79e8b
Christian D'Errico <christian.derrico@studio.unibo.it>
2021-10-02 22:08:51 +0200

    Pop up edit modified to make names unique

--- frontend/src/components/DeletePopUp.tsx
+++ frontend/src/components/DeletePopUp.tsx
@@ -2,17 +2,30 @@ import React, {createRef} from "react";
 import {Popup} from "react-leaflet";
 import {Button} from "antd";
 import * as utils from "../utils/utils"
+import {ok} from "assert";
 require("../styles/pop_up/popUpStyle.scss")
 require("../styles/pop_up/deletePopupStyle.scss")
 
 interface Props {
     room_id: string,
+    onDelete: () => void
 }
 
 const popupRef = createRef<L.Popup>()
 const closeButtonIndex: number = 2;
 
-const DeletePopUp: React.FC<Props> = ({room_id}) => {
+async function handleClose(id: string){
+    return fetch(`http://localhost:80/api/remove-room/${id}`)
+        .then(response => response.ok ? utils.closePopup(popupRef, closeButtonIndex) : null)
+}
+
+const DeletePopUp: React.FC<Props> = ({room_id, onDelete}) => {
+
+    async function handleDelete(id: string){
+        await handleClose(id)
+        onDelete();
+    }
+
     return (
         <Popup
             ref={popupRef}
@@ -20,7 +33,7 @@ const DeletePopUp: React.FC<Props> = ({room_id}) => {
             <p>Vuoi eliminare {room_id}?</p>
             <div className={"pop-up-buttons"}>
                 <Button className={"choice-button"} onClick={() => utils.closePopup(popupRef, closeButtonIndex)}>No</Button>
-                <Button className={"choice-button"}>Si</Button></div>
+                <Button className={"choice-button"} onClick={() => handleDelete(room_id)}>Si</Button></div>
         </Popup>
     )
 }
