c4d2e5670462f9341960a687f684a4b3450f43c8
Christian D'Errico <christian.derrico@studio.unibo.it>
2021-11-11 15:03:03 +0100

    css file converted to scss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router} from "react-router-dom";
-import './styles/App.css';
+import './styles/App.scss';
 import {Routes} from "./routes/routes";
 require('dotenv').config()
 
